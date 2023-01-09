fcd3aa11779d2bd11849784c7b0b6ee42f992114
chrome-ext-dev <110234832+chrome-ext-dev@users.noreply.github.com>
2023-01-10 02:51:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from theApi import getEmail
+from theApi import getEmail, randomUser
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -17,6 +17,12 @@ app.add_middleware(
 @app.get("/")
 async def read_root():
     return {'Welcome To': 'Email Reader API'}
+
+@app.get("/fakename/{gender}")
+def read_root(gender: str, request: Request):
+    theGender = str(gender)
+    user = randomUser(theGender)
+    return user
 
 @app.post("/pinterest")
 async def get_body(request: Request):
